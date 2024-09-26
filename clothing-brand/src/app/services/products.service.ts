0375fc7ff97e660715f5e4cf842294d3b09ba23a
JVC-Byte <jvc8463@gmail.com>
2024-09-26 22:24:12 +0100

    Implement crude operation

--- clothing-brand/src/app/services/products.service.ts
+++ clothing-brand/src/app/services/products.service.ts
@@ -8,12 +8,35 @@ import { PaginationParam, Products } from '../../types';
 })
 export class ProductsService {
   constructor(private apiService: ApiService) {}
+
+  // Add product to API
+  addProduct = (url: string, body: any) => {
+    return this.apiService.post(url, body, {
+      responseType: 'json',
+    });
+  };
+
+  // Get products from API
   getProducts = (
     url: string,
     params: PaginationParam
   ): Observable<Products> => {
     return this.apiService.get(url, {
       params,
+      responseType: 'json',
+    });
+  };
+
+  // Edit product from API
+  editProduct = (url: string, body: any) => {
+    return this.apiService.put(url, body, {
+      responseType: 'json',
+    });
+  };
+
+  // Delete product from API
+  deleteProduct = (url: string) => {
+    return this.apiService.delete(url, {
       responseType: 'json',
     });
   };
