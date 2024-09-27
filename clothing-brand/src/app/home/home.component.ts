40e8a1ddddac0e57be1ffd99984ee63b8730e803
JVC-Byte <jvc8463@gmail.com>
2024-09-27 21:09:08 +0100

    done with implementation of CRUD operation from UI

--- clothing-brand/src/app/home/home.component.ts
+++ clothing-brand/src/app/home/home.component.ts
@@ -1,27 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
-import { PaginatorModule } from 'primeng/paginator';
+import { Paginator, PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProductComponent, PaginatorModule],
+  imports: [CommonModule, ProductComponent, PaginatorModule, EditPopupComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productService: ProductsService) {}
+
+  @ViewChild('paginator') paginator: Paginator | undefined;
+
+
   products: Product[] = [];
   totalRecords: number = 0; 
   first: number = 0;
   rows: number = 5;
   clothes_url_address: string = 'http://localhost:3000/clothes';
 
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+ toggleEditPopup(product: Product){
+    this.selectedProduct = product;
+    this.displayEditPopup = !this.displayEditPopup;
+  }
+
+  toggleDeletePopup(product: Product){
+    if(!product.id){
+      return;
+    }
+    this.deleteProduct(product.id);
+  }
+
+  toggleAddPopup(){
+    this.displayAddPopup = !this.displayAddPopup;
+  }
+
+  onConfirmEdit(product: Product){
+    if (!this.selectedProduct.id ){
+      return;
+    }
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayEditPopup = false;
+  }
+
+  onConfirmAdd(product: Product){
+    this.addProduct(product);
+    this.displayAddPopup = false;
+  }
+
+
   onProductOutput(product: Product) {
     console.log(product, 'Output');
+  }
+
+  resetPaginator(){
+    this.paginator?.changePage(0);
   }
 
   onPageChange(event: any){
@@ -47,6 +98,8 @@ export class HomeComponent {
       next: (data) => {
         console.log(data);
         this.fetchProducts(this.first, this.rows);
+        this.resetPaginator();
+
       },
       error: (error) => {
         console.error(error);
@@ -60,6 +113,8 @@ export class HomeComponent {
       next: (data) => {
         console.log(data);
         this.fetchProducts(this.first, this.rows);
+        this.resetPaginator();
+
       },
       error: (error) => {
         console.error(error);
@@ -72,6 +127,7 @@ export class HomeComponent {
       next: (data) => {
         console.log(data);
         this.fetchProducts(this.first, this.rows);
+        this.resetPaginator();
       },
       error: (error) => {
         console.error(error);
